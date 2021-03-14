6fa4db7d0a82a0407c1657fc77c837475fedf0f8
arny617 <45241817+arny617@users.noreply.github.com>
2021-03-14 00:46:38 -0800

    Create PassingCars.py

---
+++ Codility/PassingCars.py
@@ -0,0 +1,10 @@
+### Passing Cars: Count how many times cars pass each other, West -> 1, East -> 0
+def countPassingCars(A):
+    stack = []
+    countEast = 0
+    for item in A:
+        if item==0:
+            countEast += 1
+        else:
+            stack.append(countEast)
+    return sum(stack)
